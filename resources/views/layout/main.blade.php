47229abe1d77a3a8052987c10c637d45faa93b1f
soheila <soheila.it76@gmail.com>
2020-01-04 21:12:21 -0800

    commit alaki

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -3,7 +3,8 @@
 
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 <head>
-    <title>صفحه اصلی</title>
+
+    <title>صفحه اصلی|فروشگاه مبلمان اصیل</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
