98b899ba686afc828255a8168b8e133440ac9e34
soheila <soheila.it76@gmail.com>
2020-01-27 02:01:21 +0330

    commit file

--- resources/views/memberViews/allProduct.blade.php
+++ resources/views/memberViews/allProduct.blade.php
@@ -18,8 +18,14 @@
         <div class="row">
             <div class="col-sm-6 col-md-4 col-lg-3 p-b-50">
                 <div class="leftbar p-r-20 p-r-0-sm">
-
-                    <h4 class="m-text14 p-b-7">
+                <div class="search-product pos-relative bo4 of-hidden">
+                        <input class="s-text7 size6 p-l-23 p-r-50" type="text" name="search-product"
+                            placeholder="Search Products...">
+                        <button class="flex-c-m size5 ab-r-m color2 color0-hov trans-0-4">
+                            <i class="fs-12 fa fa-search" aria-hidden="true"></i>
+                        </button>
+                    </div>
+                    <h4 class="m-text14 p-b-7 mt-5">
                         دسته بندی
                     </h4>
                     <ul class="p-b-54">
@@ -50,75 +56,23 @@
                         </li>
                     </ul>
 
-                    <h4 class="m-text14 p-b-32">
-                        فیلتر
-                    </h4>
-
-                    <div class="filter-color p-t-22 p-b-50 bo3">
-                        <div class="m-text15 p-b-12">
-                            رنگ بندی
-                        </div>
-                        <ul class="flex-w">
-                            <li class="m-r-10">
-                                <input class="checkbox-color-filter" id="color-filter1" type="checkbox"
-                                    name="color-filter1">
-                                <label class="color-filter color-filter1" for="color-filter1"></label>
-                            </li>
-                            <li class="m-r-10">
-                                <input class="checkbox-color-filter" id="color-filter2" type="checkbox"
-                                    name="color-filter2">
-                                <label class="color-filter color-filter2" for="color-filter2"></label>
-                            </li>
-                            <li class="m-r-10">
-                                <input class="checkbox-color-filter" id="color-filter3" type="checkbox"
-                                    name="color-filter3">
-                                <label class="color-filter color-filter3" for="color-filter3"></label>
-                            </li>
-                            <li class="m-r-10">
-                                <input class="checkbox-color-filter" id="color-filter4" type="checkbox"
-                                    name="color-filter4">
-                                <label class="color-filter color-filter4" for="color-filter4"></label>
-                            </li>
-                            <li class="m-r-10">
-                                <input class="checkbox-color-filter" id="color-filter5" type="checkbox"
-                                    name="color-filter5">
-                                <label class="color-filter color-filter5" for="color-filter5"></label>
-                            </li>
-                            <li class="m-r-10">
-                                <input class="checkbox-color-filter" id="color-filter6" type="checkbox"
-                                    name="color-filter6">
-                                <label class="color-filter color-filter6" for="color-filter6"></label>
-                            </li>
-                            <li class="m-r-10">
-                                <input class="checkbox-color-filter" id="color-filter7" type="checkbox"
-                                    name="color-filter7">
-                                <label class="color-filter color-filter7" for="color-filter7"></label>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="search-product pos-relative bo4 of-hidden">
-                        <input class="s-text7 size6 p-l-23 p-r-50" type="text" name="search-product"
-                            placeholder="Search Products...">
-                        <button class="flex-c-m size5 ab-r-m color2 color0-hov trans-0-4">
-                            <i class="fs-12 fa fa-search" aria-hidden="true"></i>
-                        </button>
-                    </div>
+                    
                 </div>
             </div>
             <div class="col-sm-6 col-md-8 col-lg-9 p-b-50">
 
                 <div class="flex-sb-m flex-w p-b-35">
                     <div class="flex-w">
-                        <div class="rs2-select2 bo4 of-hidden w-size12 m-t-5 m-b-5 m-r-10">
-                            <select class="selection-2" name="sorting">
+                        <div class="w-size12 m-t-5 m-b-5 m-r-10">
+                            <select class="form-control height-costum" name="sorting">
                                 <option>محصولات پیش فرض</option>
                                 <option>محصولات محبوب</option>
                                 <option>قیمت: کم به زیاد</option>
                                 <option>قیمت: زیاد به کم</option>
                             </select>
                         </div>
-                        <div class="rs2-select2 bo4 of-hidden w-size12 m-t-5 m-b-5 m-r-10">
-                            <select class="selection-2" name="sorting">
+                        <div class="w-size12 m-t-5 m-b-5 m-r-10">
+                            <select class="form-control height-costum" name="sorting">
                                 <option>قیمت</option>
                                 <option>3000000 - 5000000</option>
                                 <option>5000000 - 8000000</option>
@@ -150,23 +104,18 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="block2-txt p-t-20">
+                                    <div class="block2-txt p-t-20 text-center">
                                         <a href="{{route('singleProduct',$allProductMember->id)}}" class="block2-name dis-block s-text3 p-b-5">
-                                            {{$allProductMember->name}}
+                                           {{$allProductMember->name}}
                                         </a>
                                         <span class="block2-price m-text6 p-r-5">
-                                    {{$allProductMember->pastcost}} {{$allProductMember->lastcost}}
+                                  <s>{{$allProductMember->pastcost}}</s> <br>{{$allProductMember->lastcost}}
                                 </span>
                                     </div>
                                 </div>
                             </div>
                         @endforeach
                     @endif
-                </div>
-
-                <div class="pagination flex-m flex-w p-t-26">
-                    <a href="#" class="item-pagination flex-c-m trans-0-4 active-pagination">1</a>
-                    <a href="#" class="item-pagination flex-c-m trans-0-4">2</a>
                 </div>
             </div>
         </div>
