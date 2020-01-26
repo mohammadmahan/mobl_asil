c3004fdf8facf4a7215760e4da39a908a9dd642f
mohammad shaddel <mohammad.shaddel130@gmail.com>
2020-01-26 13:11:49 +0330

    مدال حذف پیام ها درست شد

--- resources/views/ManagementViews/site/massages.blade.php
+++ resources/views/ManagementViews/site/massages.blade.php
@@ -44,6 +44,29 @@
                                         </td>
                                     </tr>
 
+                                    <!------------------start modal delete----------------------------->
+                                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+                                         aria-hidden="true">
+                                        <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="exampleModalLabel">حذف پیام</h5>
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    ایا میخواهید حذف کنید؟
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">بستن</button>
+                                                    <a href="{{route('delete.massage',$massage->id)}}" type="submit" class="btn btn-primary">حذف</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!------------------end modal delete----------------------------->
+
                                     @endforeach
                                     @endif
                                 </tbody>
@@ -55,28 +78,7 @@
         </div>
         <!-- Table head options end -->
 
-        <!------------------start modal delete----------------------------->
-        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-            aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">حذف پیام</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                       ایا میخواهید حذف کنید؟
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">بستن</button>
-                        <button href="{{route('delete.massage',$massage->id)}}" type="submit" class="btn btn-primary">حذف</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!------------------end modal delete----------------------------->
+
     </div>
 </div>
 
